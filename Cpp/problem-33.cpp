7fea9b534d112ef392f12464aebb078697178c41
kwonta3wook <152182535+Kowntaewook@users.noreply.github.com>
2025-11-25 16:38:02 +0900

    Add files via upload

---
+++ Cpp/problem-33.cpp
@@ -0,0 +1,86 @@
+/*문제3) 바이러스 (초등3)
+
+신종 바이러스인 웜 바이러스는 네트워크를 통해 전파된다. 한 컴퓨터가 웜 바이러스에 걸리면 그 컴퓨터와 네트워크 상에서 연결되어 있는 모든 컴퓨터는 웜 바이러스에 걸리게 된다.
+
+예를 들어 7대의 컴퓨터가 <그림 1>과 같이 네트워크 상에서 연결되어 있다고 하자. 1번 컴퓨터가 웜 바이러스에 걸리면 웜 바이러스는 2번과 5번 컴퓨터를 거쳐 3번과 6번 컴퓨터까지 전파되어 2, 3, 5, 6 네 대의 컴퓨터는 웜 바이러스에 걸리게 된다. 하지만 4번과 7번 컴퓨터는 1번 컴퓨터와 네트워크상에서 연결되어 있지 않기 때문에 영향을 받지 않는다.
+
+
+
+어느 날 1번 컴퓨터가 웜 바이러스에 걸렸다. 컴퓨터의 수와 네트워크 상에서 서로 연결되어 있는 정보가 주어질 때, 1번 컴퓨터를 통해 웜 바이러스에 걸리게 되는 컴퓨터의 수를 출력하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에는 컴퓨터의 수가 주어진다. 컴퓨터의 수는 100 이하이고 각 컴퓨터에는 1번 부터 차례대로 번호가 매겨진다.
+
+둘째 줄에는 네트워크 상에서 직접 연결되어 있는 컴퓨터 쌍의 수가 주어진다.
+
+이어서 그 수만큼 한 줄에 한 쌍씩 네트워크 상에서 직접 연결되어 있는 컴퓨터의 번호 쌍이 주어진다.
+
+출력
+1번 컴퓨터가 웜 바이러스에 걸렸을 때, 1번 컴퓨터를 통해 웜 바이러스에 걸리게 되는 컴퓨터의 수를 첫째 줄에 출력한다.
+
+입력 예시   
+7
+6
+1 2
+2 3
+1 5
+5 2
+5 6
+4 7
+
+출력 예시
+4 */
+
+// use Disjoint sets
+#include <stdio.h>
+#include <vector>
+using namespace std;
+
+int compnum, netnum;
+int parent[101];
+vector<pair<int, int>> networks;
+
+// find
+int findParent(int x)
+{
+    if(x == parent[x])
+        return x;
+    return parent[x] = findParent(parent[x]);
+}
+
+// union
+void unionParent(int a, int b)
+{
+    a = findParent(a);
+    b = findParent(b);
+    if(a < b)
+        parent[b] = a;
+    else
+        parent[a] = b;
+}
+
+int main()
+{
+    scanf("%d", &compnum);
+    scanf("%d", &netnum);
+
+    for(int i = 1; i <= compnum; i++)
+        parent[i] = i;
+    
+    for(int i = 0; i < netnum; i++)
+    {
+        int a, b;
+        scanf("%d %d", &a, &b);
+        unionParent(a, b);
+    }
+    int virus_count = 0;
+    for(int i = 2; i <= compnum; i++)
+    {
+        if(findParent(1) == findParent(i))
+            virus_count++;
+        else
+            continue;
+    }
+    printf("%d\n", virus_count);
+    return 0;
+}
